efbff8ccc123ade6275e52c8c526b75a78d28ac6
Ed <edmundluong@gmail.com>
2019-01-07 23:37:03 -0800

    enhanced syntax in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,22 @@
 //just need to call the specific function to populate the results
 import 'package:flutter/material.dart'; // need this to import Material class to draw widget to screen
 
-main() {
-  runApp(MyApp()); //need to mount the widget
-} 
+// void main() {
+//   runApp(MyApp()); //need to mount the widget
+// } 
+
+void main() => runApp(MyApp()); //equivalent to the statement above
+                                //NOTE: can only be used with only one statement
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override                          //Can add annotation; telling Dart to override this method
+  Widget build(BuildContext context) { //can add Widget before build() to make sure a Widget object is returned
     // required because it will need to draw it into the screen
     // Scaffold Widget - from Material package. ship with a nice background and add an app bar.
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
+    return MaterialApp( //root widget
+      home: Scaffold( //background widget
+        appBar: AppBar( //creates the blue bar widget
+          title: Text('EasyList'), //shows the title on the blue bar widget
         ),
       ),
     ); //a widget would need to return a widget; MaterialApp() is the root widget
